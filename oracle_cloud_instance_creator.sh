ea4ef9cc0e1d85cf41ed68ea13f81ec315645d31
sentisso <38100632+sentisso@users.noreply.github.com>
2025-07-14 11:13:13 +0200

    Update oracle_cloud_instance_creator.sh

--- oracle_cloud_instance_creator.sh
+++ oracle_cloud_instance_creator.sh
@@ -27,24 +27,35 @@ cpus=4 # max 4 cores
 ram=24 # max 24gb memory
 bootVolume=50 # disk size in gb
 
-profile="DEFAULT"
-
 # ----------------------ENDLESS LOOP TO REQUEST AN ARM INSTANCE---------------------------------------------------------
 
+IFS=',' read -ra AD_ARRAY <<< "$AVAILABILITY_DOMAIN"
+
 while true; do
+    for AD in "${AD_ARRAY[@]}"; do
+        echo "Launching instance in AD: $AD"
 
-    oci compute instance launch --no-retry  \
-    --auth api_key \
-    --profile "$profile" \
-    --display-name big-arm \
-    --compartment-id "$TENANCY_ID" \
-    --image-id "$IMAGE_ID" \
-    --subnet-id "$SUBNET_ID" \
-    --availability-domain "$AVAILABILITY_DOMAIN" \
-    --shape 'VM.Standard.A1.Flex' \
-    --shape-config "{'ocpus':$cpus,'memoryInGBs':$ram}" \
-    --boot-volume-size-in-gbs "$bootVolume" \
-    --ssh-authorized-keys-file "$PATH_TO_PUBLIC_SSH_KEY"
+        oci compute instance launch --no-retry \
+        --auth api_key \
+        --profile "$PROFILE" \
+        --display-name $DISPLAY_NAME \
+        --compartment-id "$TENANCY_ID" \
+        --image-id "$IMAGE_ID" \
+        --subnet-id "$SUBNET_ID" \
+        --availability-domain "$AD" \
+        --shape 'VM.Standard.A1.Flex' \
+        --shape-config "{\"ocpus\":$cpus,\"memoryInGBs\":$ram}" \
+        --boot-volume-size-in-gbs "$bootVolume" \
+        --ssh-authorized-keys-file "$PATH_TO_PUBLIC_SSH_KEY"
 
-    sleep $requestInterval
+        # Check if the command was successful
+        if [ $? -eq 0 ]; then
+            echo "Instance created successfully! Exiting."
+            break
+        else
+            echo "Instance creation failed. Retrying in $requestInterval seconds..."
+        fi
+
+        sleep "$requestInterval"
+    done
 done
